c1c26c8716e9b4a5665d4e25b27d42c5cee4be5d
scardinius <scardinius@chords.pl>
2018-02-16 16:04:04 +0100

    Add method for setting utm custom fields for given recurring contribution

--- CRM/Contributm/Model/UtmRecur.php
+++ CRM/Contributm/Model/UtmRecur.php
@@ -1,6 +1,6 @@
 <?php
 
-class CRM_Contributm_Model_Fields {
+class CRM_Contributm_Model_UtmRecur {
 
   const CUSTOM_GROUP_NAME_RECUR_UTM = 'recur_utm';
   const CUSTOM_FIELD_PREFIX = 'custom_';
@@ -21,6 +21,8 @@ class CRM_Contributm_Model_Fields {
   }
 
   /**
+   * Get custom field name for utm_source.
+   *
    * @return string
    * @throws \CiviCRM_API3_Exception
    */
@@ -36,6 +38,8 @@ class CRM_Contributm_Model_Fields {
   }
 
   /**
+   * Get custom field name for utm_medium.
+   *
    * @return string
    * @throws \CiviCRM_API3_Exception
    */
@@ -51,6 +55,8 @@ class CRM_Contributm_Model_Fields {
   }
 
   /**
+   * Get custom field name for utm_campaign.
+   *
    * @return string
    * @throws \CiviCRM_API3_Exception
    */
@@ -66,6 +72,8 @@ class CRM_Contributm_Model_Fields {
   }
 
   /**
+   * Get custom field name for utm_content.
+   *
    * @return string
    * @throws \CiviCRM_API3_Exception
    */
@@ -78,6 +86,38 @@ class CRM_Contributm_Model_Fields {
       return $id;
     }
     return $cache;
+  }
+
+
+  /**
+   * Set utm custom fields for given recurring contribution.
+   *
+   * @param int $recurringId
+   * @param array $fields
+   *
+   * @throws \CiviCRM_API3_Exception
+   */
+  function set($recurringId, $fields = []) {
+    $params = array(
+      'sequential' => 1,
+      'entity_id' => $recurringId,
+      'entity_table' => 'civicrm_contribution_recur',
+    );
+    if (CRM_Utils_Array::value('utm_source', $fields)) {
+      $params[self::utmSource()] = $fields['utm_source'];
+    }
+    if (CRM_Utils_Array::value('utm_medium', $fields)) {
+      $params[self::utmMedium()] = $fields['utm_medium'];
+    }
+    if (CRM_Utils_Array::value('utm_campaign', $fields)) {
+      $params[self::utmCampaign()] = $fields['utm_campaign'];
+    }
+    if (CRM_Utils_Array::value('utm_content', $fields)) {
+      $params[self::utmContent()] = $fields['utm_content'];
+    }
+    if (count($params) > 3) {
+      civicrm_api3('CustomValue', 'create', $params);
+    }
   }
 
 }
