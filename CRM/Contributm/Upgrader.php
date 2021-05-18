55b519b57bc94f2517db98aaa119a6bc78bb0c44
Romain Thouvenin <romain@wemove.eu>
2021-05-18 16:55:46 +0200

    Declare utm custom group

--- CRM/Contributm/Upgrader.php
+++ CRM/Contributm/Upgrader.php
@@ -4,11 +4,14 @@
  * Collection of upgrade steps.
  */
 class CRM_Contributm_Upgrader extends CRM_Contributm_Upgrader_Base {
-  /**
-   * @return bool
-   */
+
   public function upgrade_110_install_custom_recur_utm() {
     $this->executeCustomDataFileByAbsPath($this->extensionDir . '/xml/recur_utm.xml');
+    return TRUE;
+  }
+
+  public function upgrade_120_install_custom_utm() {
+    $this->executeCustomDataFileByAbsPath($this->extensionDir . '/xml/utm.xml');
     return TRUE;
   }
 
